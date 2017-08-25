e0c6560856cee6aaf902047b2b28aa1827b8fa42
Pavel <ekonomikal@mail.ru>
2017-08-25 20:51:21 +0300

    Bash scripts

---
+++ mysqltool.conf.php
@@ -0,0 +1,8 @@
+
+<?php
+return array(
+    "remote" => array(
+        "sshhost" => "dnovikov32@print.hardnig.ga",
+        "folder" => "/home/pavel/Документы/DEVELOP/FL/press/public_html"
+    )
+);
