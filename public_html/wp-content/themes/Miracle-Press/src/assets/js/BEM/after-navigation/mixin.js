13c15576afdc3e637013915570c0b2a1d43400d2
Pavel <ekonomikal@mail.ru>
2017-08-27 20:03:38 +0300

    Edit header block for pixel

--- public_html/wp-content/themes/Miracle-Press/src/assets/js/BEM/after-navigation/mixin.js
+++ public_html/wp-content/themes/Miracle-Press/src/assets/js/BEM/after-navigation/mixin.js
@@ -1,13 +1,14 @@
 import $ from 'jquery';
 
 var on_center = function(){
+	var m = parseFloat( $('.after-navigation__button').css('margin-left') );
 	var w_after_nav = $('.after-navigation').width();
 	var w_text_logo = $('.after-navigation__text-logo').outerWidth();
-	var w_requer    = $('.after-navigation__requer').width();
+	var w_requer    = $('.after-navigation__requer').outerWidth();
 	var w_cont_head = $('.after-navigation__contacts-header').outerWidth();
-	var w_button    = $('.after-navigation__button').outerWidth();
-	if( ( w_cont_head + w_button ) < ( ( w_after_nav - w_requer - 50 ) / 2 ) ){
-		var width = ( w_after_nav - w_requer ) / 2 - w_text_logo;
+	var w_button    = $('.after-navigation__button').outerWidth() + m;
+	if( ( w_cont_head + w_button + 72 ) < ( ( w_after_nav - w_requer - 50 ) / 2 ) ){
+		var width = ( w_after_nav - w_requer ) / 2 - w_text_logo + 36;
 		$('.after-navigation__requer').css('margin-left', width);
 	}else if( ( w_text_logo + w_requer + w_cont_head + w_button ) < w_after_nav ){
 		var width = ( w_after_nav - w_text_logo - w_requer - w_cont_head - w_button  ) / 2;
