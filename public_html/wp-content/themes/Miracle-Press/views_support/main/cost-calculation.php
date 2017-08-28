9914c0cc77b62aa76fa3fa57bf11f57c5fd57d0e
Pavel <ekonomikal@mail.ru>
2017-08-29 02:27:58 +0300

    Start cost calculation block

--- public_html/wp-content/themes/Miracle-Press/views_support/main/cost-calculation.php
+++ public_html/wp-content/themes/Miracle-Press/views_support/main/cost-calculation.php
@@ -1,6 +1,6 @@
-<div id="cost-calculation" class="cost-calculation" style="background-color: green; padding: 2rem">
+<div id="cost-calculation" class="cost-calculation" style="background-image: url('<?= get_template_directory_uri() ?>/dist/assets/images/cost-calculation-bg.jpg')">
 
-	<div class="cost-calculation__wrap-form wrap-form" style="max-width: 1180px; margin: 0 auto;"">
+	<div class="cost-calculation__wrap-form wrap-form">
 		<div class="wrap-form__form-title form-title">
 			<h3 class="form-title__title">Получите расчет стоимости - Это бесплатно!</h3>
 			<p class="form-title__subtitle">Все поля со знаком <span class="form-title__subtitle_color">*</span> обязательны для заполнения</p>
@@ -9,21 +9,39 @@
 			<form action="javascript:void(0)">
 
 				<div class="input input_width-medium input_padding-right">
-					<label for="aaa" class="input__label">Введите Ваш телефон:<span class="input__label_color">*</span></label>
-					<input id="aaa" class="input__text-input" type="text" placeholder="+7 (123) 456 789 44">
+					<label for="aaa3" class="input__label">Введите Ваш телефон:<span class="input__label_color">*</span></label>
+					<input id="aaa3" class="input__text-input" type="text" placeholder="+7 (123) 456 789 44">
 					<p class="input__text-help">Чтобы мы могли связаться с Вами в течении <span class="input__text-help_color">15 минут</span></p>
 				</div>
 
 				<div class="input input_width-medium input_padding-right">
-					<label for="ccc" class="input__label">Введите Ваш email:</label>
-					<input id="ccc" class="input__text-input" type="email" placeholder="Vashapochta@mail.ru">
+					<label for="ccc3" class="input__label">Введите Ваш email:</label>
+					<input id="ccc3" class="input__text-input" type="email" placeholder="Vashapochta@mail.ru">
 					<p class="input__text-help">Отправить на него смету.</p>
 				</div>
 
 				<div class="input input_width-big">
-					<label for="bbb" class="input__label"></label>
-					<input id="bbb" class="input__submit" type="submit" value="Хочу получить расчет стоимости">
+					<label for="bbb3" class="input__label"></label>
+					<input id="bbb3" class="input__submit input__submit_yellow" type="submit" value="Хочу получить расчет стоимости">
 					<p class="input__text-help">Когда вы заполняете телефон и другие данные, Вы автоматически соглашаетесь с <a href="#" class="input__text-help_link">правилами обработки персональных данных</a></p>
+				</div>
+
+				<div class="form__edition edition">
+
+					<div class="expanded-button">
+						<button class="expanded-button__button">Показать подробнее</button>
+					</div>
+				
+					<div class="edition__form-title form-title">
+						<h3 class="form-title__title">Дополнительная информация</h3>
+						<p class="form-title__subtitle">Если на какой то пункт, затрудняетеся ответить, то просто оставьте его пустым.</p>
+					</div>
+
+					<div class="edition__textarea textarea">
+						<label for="ccc4" class="textarea__label">Описание задачи:</label>
+						<textarea id="ccc4" class="textarea__text-input" placeholder="Нужно напечатать рекламную продукцию к открытию магазина (визитки, буклеты), а так же сделать световую вывеску"></textarea>
+					</div>
+				
 				</div>
 
 			</form>
