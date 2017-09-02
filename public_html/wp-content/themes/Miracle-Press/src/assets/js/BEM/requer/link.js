bb5bd66db430baabf526435e45e479d0761efd63
Pavel <ekonomikal@mail.ru>
2017-09-02 20:13:00 +0300

    Add unfocus event

--- public_html/wp-content/themes/Miracle-Press/src/assets/js/BEM/requer/link.js
+++ public_html/wp-content/themes/Miracle-Press/src/assets/js/BEM/requer/link.js
@@ -4,7 +4,7 @@ var open_tech_requerement_modal = function(event){
 	event.preventDefault();
 	$('.modal').addClass('modal_show');
 	$('.modal').data('open', 'tech-requerement');
-	$('.modal__tech-requerement').addClass('modal__tech-requerement_show');
+	$('.modal__tech-requerement').addClass('modal__tech-requerement_show').focus();
 }
 
 var close_tech_requerement_modal = function(event){
@@ -14,5 +14,18 @@ var close_tech_requerement_modal = function(event){
 	$('.modal__' + modal).removeClass('modal__' + modal + '_show');
 }
 
+var onfocus_tech_requerement_modal = function(event){
+	var modal = $('.modal').data('open');
+	var container = $('.modal__' + modal);
+
+	if (container.has(event.target).length === 0){
+
+		$('.modal').removeClass('modal_show');
+		$('.modal__' + modal).removeClass('modal__' + modal + '_show');
+
+	}
+}
+
 $('.requer__link').on( 'click', open_tech_requerement_modal );
 $('.tech-requerement__close-button').on( 'click', close_tech_requerement_modal );
+$('.modal').bind('click', onfocus_tech_requerement_modal);
