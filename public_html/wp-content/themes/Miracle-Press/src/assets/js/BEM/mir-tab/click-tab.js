423b6b64f64c2db3562cd19e100d12286f391420
Pavel <ekonomikal@mail.ru>
2017-09-05 09:15:10 +0300

    Add frond-end question section

---
+++ public_html/wp-content/themes/Miracle-Press/src/assets/js/BEM/mir-tab/click-tab.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+
+var init_tab = function(){
+	$('.mir-tabs').find('.mir-tab').each(function(index,item){
+		$(item).data('active', false);
+		var h = $(item).find('.mir-tab__question').outerHeight();
+		var ih = $(item).find('.mir-tab__icon').height();
+		var iw = $(item).find('.mir-tab__icon').width();
+		var pv = ( h - ih - 2 ) / 2;
+		var ph = ( h - iw - 1 ) / 2;
+		$(item).find('.mir-tab__icon').css('padding', pv + 'px ' + ph + 'px' );
+		$(item).find('.mir-tab__question').css( 'width', 'calc( 100% - 2px - ' + h + 'px )' );
+	});
+}
+
+$(document).ready( init_tab );
+
+var click_tab = function(){
+	var tab = this;
+	$(this).find('.mir-tab__answer').slideToggle(300);
+	$(this).find('.mir-tab__icon').find('i').toggleClass('miracle-icon-chevron-down miracle-icon-up-chevron');
+	if( $(this).data('active') ){
+		$(this).data('active',false);
+	}else{
+		$(this).data('active',true);
+	}
+}
+
+$('.mir-tab').on( 'click', click_tab );
--- public_html/wp-content/themes/Miracle-Press/views_support/main/question.php
+++ public_html/wp-content/themes/Miracle-Press/views_support/main/question.php
@@ -6,13 +6,54 @@
 
 	<div class="question__mir-tabs mir-tabs">
 
-		<div class="mir-tab">
-			<button class="mir-tab__question">По какому принципу работает доставка</button>
+		<div class="mir-tab mir-tab_margin-right">
+			<button class="mir-tab__question">По какому принципу работает доставка?</button>
 			<button class="mir-tab__icon"><i class="miracle-icon-chevron-down"></i></button>
 			<p class="mir-tab__answer">
 				
 			</p>
 		</div>
+
+		<div class="mir-tab">
+			<button class="mir-tab__question">Могу ли я заказать монтаж вывески, если она уже есть?</button>
+			<button class="mir-tab__icon"><i class="miracle-icon-chevron-down"></i></button>
+			<p class="mir-tab__answer">
+				
+			</p>
+		</div>
+
+		<div class="mir-tab mir-tab_margin-right">
+			<button class="mir-tab__question">Если мне не понравился результат, как мне быть?</button>
+			<button class="mir-tab__icon"><i class="miracle-icon-chevron-down"></i></button>
+			<p class="mir-tab__answer">
+				
+			</p>
+		</div>
+
+		<div class="mir-tab">
+			<button class="mir-tab__question">Какие материалы используются и на сколько они долговечны?</button>
+			<button class="mir-tab__icon"><i class="miracle-icon-chevron-down"></i></button>
+			<p class="mir-tab__answer">
+				
+			</p>
+		</div>
+
+		<div class="mir-tab mir-tab_margin-right">
+			<button class="mir-tab__question">Возможна ли доставка с отсрочкой?</button>
+			<button class="mir-tab__icon"><i class="miracle-icon-chevron-down"></i></button>
+			<p class="mir-tab__answer">
+				
+			</p>
+		</div>
+
+		<div class="mir-tab">
+			<button class="mir-tab__question">Какой тираж у вас максимально можно заказать?</button>
+			<button class="mir-tab__icon"><i class="miracle-icon-chevron-down"></i></button>
+			<p class="mir-tab__answer">
+				У нас свое производство и достаточно много аппаратуры и мы можем брать очень большие объемы более 10 000 копий
+			</p>
+		</div>
+
 	</div>
 
 </div>
